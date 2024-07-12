78efc907e39bd019f442475a3ea97e395fd28fc6
John Andrei <tacujan.andre@gmail.com>
2024-07-12 20:58:58 +0800

    pre load

--- app/dashboard/content/(components)/OutputSection.tsx
+++ app/dashboard/content/(components)/OutputSection.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 
 const OutputSection = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World! 🌎️</p>",
+    editorProps: {
+      attributes: {
+        class:
+          "bg-background p-4 rounded-md shadow-sm focus:shadow-lg focus:ring-2 focus:ring-blue-500 focus:outline-none h-full border ",
+      },
+    },
+  });
+
   return (
-    <div>Output</div>
+    <div className=' h-full w-full '>
+      
+      <EditorContent editor={editor} className="h-full w-full" />
+    </div>
   )
 }
 
-export default OutputSection
+export default OutputSection;
--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import TemplateListData, { TEMPLATE } from "@/app/(data)/TemplateListData";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { ArrowLeftIcon } from "lucide-react";
+import { ArrowLeftIcon, CopyIcon } from "lucide-react";
 import FormSection from "../(components)/FormSection";
 import OutputSection from "../(components)/OutputSection";
 import { FormProvider } from "@/components/context/FormContext";
@@ -20,19 +20,28 @@ const CreateContent = ({ params }: PROPS) => {
 
   return (
     <div className="bg-card">
-      <Button asChild className="m-2">
-        <Link href="/dashboard">
-          <ArrowLeftIcon />
-          Back
-        </Link>
-      </Button>
+      <div className="flex  justify-between">
+        <Button asChild className="m-2">
+          <Link href="/dashboard">
+            <ArrowLeftIcon />
+            Back
+          </Link>
+        </Button>
+        <Button asChild className="m-2">
+          <Link href="/dashboard">
+            Copy
+            <CopyIcon />
+          </Link>
+        </Button>
+      </div>
+
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 p-5 h-full">
         <div className="col-span-1">
-          <FormProvider >
+          <FormProvider>
             <FormSection selectedTemplate={selectedTemplate} />
           </FormProvider>
         </div>
-        <div className="col-span-2">
+        <div className="col-span-2 h-full flex">
           <OutputSection />
         </div>
       </div>
