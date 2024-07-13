7bd2cad2be4e1dafab3c092d16da61b47ff36dfb
John Andrei <tacujan.andre@gmail.com>
2024-07-14 01:34:01 +0800

    added: table shadcn

--- app/dashboard/history/page.tsx
+++ app/dashboard/history/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Payment, columns } from "./(components)/columns";
+import { DataTable } from "./(components)/data-table";
 
-const History = () => {
-  return (
-    <div>History</div>
-  )
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
 }
 
-export default History
+const History = async () => {
+  const data = await getData();
+
+  return (
+    <div className="bg-card h-full w-full p-5">
+      <div className="bg-background rounded-lg p-5">
+        <div>
+          <h1 className=" font-extrabold text-2xl">History</h1>
+          <p className="text-gray-500 font-serif">
+            View previously generated AI content.
+          </p>
+        </div>
+        <div>
+          <DataTable columns={columns} data={data} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default History;
