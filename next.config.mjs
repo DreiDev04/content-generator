131c36c247030a1df104841c6a85f2f36a59ffb2
John Andrei <tacujan.andre@gmail.com>
2024-07-17 02:57:42 +0800

    added: next env

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     DRIZZLE_DATABASE_URL: process.env.DRIZZLE_DATABASE_URL,
 
